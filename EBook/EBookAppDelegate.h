e37718c0912467adc31cee522033bbba8ad89470
JasonZz <122154413@qq.com>
2012-11-27 14:49:30 +0800

    Initial Commit

---
+++ EBook/EBookAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EBookAppDelegate.h
+//  EBook
+//
+//  Created by Jason on 12-11-27.
+//  Copyright (c) 2012年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBookAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EBook
+//
+//  Created by Jason on 12-11-27.
+//  Copyright (c) 2012年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EBookAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EBookAppDelegate class]));
+    }
+}
